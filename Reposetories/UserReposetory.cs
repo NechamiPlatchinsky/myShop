7e3d762f2d810600a0fc74baaf79464fa13b050e
NechamiPlatchinsky <38214416448@mby.co.il>
2024-12-01 20:25:49 +0200

    data first

--- Reposetories/UserReposetory.cs
+++ Reposetories/UserReposetory.cs
@@ -28,51 +28,14 @@ namespace Reposetories
         }
         public async Task<User> getUserToLogin(string Email, string Password)
         {
-            //using (StreamReader reader = System.IO.File.OpenText("M:/web api/myShop/myShop/FileUser.txt"))
-            //{//
-
-            //    string? currentUserInFile;
-            //    while ((currentUserInFile = reader.ReadLine()) != null)
-            //    {
-            //        User user = JsonSerializer.Deserialize<User>(currentUserInFile);
-            //        if (user.Email == Email && user.Password == Password)
-            //            return user;
-            //    }
-            //}
-            //return null;
-            //return await myShopDBContext.Users.FirstOrDefaultAsync(user => user.Email == Email && user.Password == Password);
-
-
            return await myShopDBContext.Users.FirstOrDefaultAsync(user => user.Email == Email && user.Password == Password);
-            
-            //return user;
         }
 
         public async Task updateUser(int id, User updateUser)
         {
             updateUser.UserId = id;
-            //string textToReplace = string.Empty;
-            //using (StreamReader reader = System.IO.File.OpenText("M:/web api/myShop/myShop/FileUser.txt"))
-            //{
-            //    string currentUserInFile;
-            //    while ((currentUserInFile = reader.ReadLine()) != null)
-            //    {
-
-            //        User user = JsonSerializer.Deserialize<User>(currentUserInFile);
-            //        if (user.UserId == id)
-            //            textToReplace = currentUserInFile;
-            //    }
-            //}
-
-            //if (textToReplace != string.Empty)
-            //{
-            //    string text = System.IO.File.ReadAllText("M:/web api/myShop/myShop/FileUser.txt");
-            //    text = text.Replace(textToReplace, JsonSerializer.Serialize(updateUser));
-            //    System.IO.File.WriteAllText("M:/web api/myShop/myShop/FileUser.txt", text);
-            //}
             myShopDBContext.Users.Update(updateUser);
             await myShopDBContext.SaveChangesAsync();
-            //return updateUser;
         }
     }
 }
